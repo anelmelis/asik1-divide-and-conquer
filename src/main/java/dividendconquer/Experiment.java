7b03ca0e12c38165c7bbb99bb7b8a8c8bfda0df0
Anel Melis <anel@example.com>
2026-09-20 15:50:26 +0500

    results to csv

--- src/main/java/dividendconquer/Experiment.java
+++ src/main/java/dividendconquer/Experiment.java
@@ -1,66 +1,92 @@
 package dividendconquer;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 
 public class Experiment {
     public static void main(String[] args) {
-        int[] sizes = {100, 500, 1000, 5000, 10000};
-        String[] types = {"random", "sorted", "reverse", "duplicates"};
-        Random random = new Random();
+        try {
+            FileWriter writer = new FileWriter("results/results.csv");
 
-        System.out.println("Algorithm experiments");
-        System.out.println();
+            writer.write("algorithm,input_type,size,time_ns,recursion_depth,comparisons,swaps\n");
 
-        for (String type : types) {
-            System.out.println("Input type: " + type);
-            for (int size : sizes) {
-                int[] a = new int[size];
+            int[] sizes = {100, 500, 1000, 5000, 10000};
+            String[] types = {"random", "sorted", "reverse", "duplicates"};
+            Random random = new Random();
 
-                for (int i = 0; i < size; i++) {
-                    if (type.equals("random")) {
-                        a[i] = random.nextInt(100000);
-                    } else if (type.equals("sorted")) {
-                        a[i] = i;
-                    } else if (type.equals("reverse")) {
-                        a[i] = size - i;
-                    } else {
-                        a[i] = random.nextInt(10);
+            System.out.println("Algorithm experiments");
+            System.out.println();
+
+            for (String type : types) {
+                System.out.println("Input type: " + type);
+                for (int size : sizes) {
+                    int[] a = new int[size];
+
+                    for (int i = 0; i < size; i++) {
+                        if (type.equals("random")) {
+                            a[i] = random.nextInt(100000);
+                        } else if (type.equals("sorted")) {
+                            a[i] = i;
+                        } else if (type.equals("reverse")) {
+                            a[i] = size - i;
+                        } else {
+                            a[i] = random.nextInt(10);
+                        }
                     }
+
+
+                    int[] mergeArray = a.clone();
+                    MergeSorter mergeSorter = new MergeSorter();
+
+                    long start = System.nanoTime();
+                    mergeSorter.sort(mergeArray);
+                    long end = System.nanoTime();
+
+                    System.out.println(
+                            "MergeSort: n=" + size +
+                                    ", time=" + (end - start) +
+                                    " ns, depth=" + mergeSorter.getMaxDepth() +
+                                    ", comparisons=" + mergeSorter.getComparisons()
+                    );
+                    writer.write(
+                            "MergeSort," + type + "," + size + "," +
+                                    (end - start) + "," +
+                                    mergeSorter.getMaxDepth() + "," +
+                                    mergeSorter.getComparisons() + ",0\n"
+                    );
+
+
+                    int[] quickArray = a.clone();
+                    QuickSorter quickSorter = new QuickSorter();
+
+                    start = System.nanoTime();
+                    quickSorter.sort(quickArray);
+                    end = System.nanoTime();
+
+                    System.out.println(
+                            "QuickSort: n=" + size +
+                                    ", time=" + (end - start) +
+                                    " ns, depth=" + quickSorter.getMaxDepth() +
+                                    ", comparisons=" + quickSorter.getComparisons() +
+                                    ", swaps=" + quickSorter.getSwaps()
+                    );
+                    writer.write(
+                            "QuickSort," + type + "," + size + "," +
+                                    (end - start) + "," +
+                                    quickSorter.getMaxDepth() + "," +
+                                    quickSorter.getComparisons() + "," +
+                                    quickSorter.getSwaps() + "\n"
+                    );
                 }
 
-
-                int[] mergeArray = a.clone();
-                MergeSorter mergeSorter = new MergeSorter();
-
-                long start = System.nanoTime();
-                mergeSorter.sort(mergeArray);
-                long end = System.nanoTime();
-
-                System.out.println(
-                        "MergeSort: n=" + size +
-                                ", time=" + (end - start) +
-                                " ns, depth=" + mergeSorter.getMaxDepth() +
-                                ", comparisons=" + mergeSorter.getComparisons()
-                );
-
-
-                int[] quickArray = a.clone();
-                QuickSorter quickSorter = new QuickSorter();
-
-                start = System.nanoTime();
-                quickSorter.sort(quickArray);
-                end = System.nanoTime();
-
-                System.out.println(
-                        "QuickSort: n=" + size +
-                                ", time=" + (end - start) +
-                                " ns, depth=" + quickSorter.getMaxDepth() +
-                                ", comparisons=" + quickSorter.getComparisons() +
-                                ", swaps=" + quickSorter.getSwaps()
-                );
+                System.out.println();
             }
+            writer.close();
 
-            System.out.println();
+        } catch (IOException e) {
+            System.out.println("Error writing CSV: " + e.getMessage());
         }
-    }
+        }
+
 
 }
